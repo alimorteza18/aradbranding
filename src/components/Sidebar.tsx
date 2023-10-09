90911f65194e474ea1bc1c7bbb3155d2d161bb84
Darkness <alimurtaza7x@gmail.com>
2023-10-09 15:49:10 +0330

    new-update

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { MdDashboardCustomize, MdSettings } from "react-icons/md"
 import { CgList } from "react-icons/cg"
 import { FaUsers } from "react-icons/fa"
-import { IoIosLogIn } from "react-icons/io"
+import {AiOutlineComment} from "react-icons/ai"
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ThemeSwitcher } from ".";
@@ -15,6 +15,7 @@ const Sidebar = ({ children }: any) => {
     const [dropDown, setDropdown] = useState<boolean>(false);
     const [showUser, setShowUser] = useState<boolean>(false);
     const pathName = usePathname();
+
     const openSideBar = () => {
         setShowSideBAr(!showSideBar);
     };
@@ -41,22 +42,22 @@ const Sidebar = ({ children }: any) => {
                             </button>
                             <a href="https://aradbranding.com" className="flex ">
                                 <Image src="/images/arad.svg" width={35} height={35} className="h-8 ml-3" alt="Arad Branfing Logo" />
-                                <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Arad Branding</span>
+                                <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white hidden sm:block">Arad Branding</span>
                             </a>
-                            <ThemeSwitcher />
                         </div>
                         <div className="flex items-center">
+                            <ThemeSwitcher />
                             <div className="flex items-center mr-3">
                                 <div>
                                     <button onClick={openUser} type="button" className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
                                         <span className="sr-only">Open user menu</span>
-                                        <Image className="w-8 h-8 rounded-full" src="/images/avatar.png" width={100} height={100} alt="Avatar" />
+                                        <img className="w-8 h-8 rounded-full" src="https://wallpapers-clan.com/wp-content/uploads/2023/02/anime-boy-black-pfp-2.jpg" alt="Avatar" />
                                     </button>
                                 </div>
-                                {/* <div className={`z-50 ${showUser ? `` : `hidden`} my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600`} id="dropdown-user">
+                                <div className={`z-50 ${showUser ? `` : `hidden`} absolute left-9 top-14 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600`} id="dropdown-user">
                                     <div className="px-4 py-3" role="none">
                                         <p className="text-sm text-gray-900 dark:text-white" role="none">
-                                            Ali Morteza
+                                            علی مرتضی یلمازتکین
                                         </p>
                                         <p className="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
                                             alimurtaza7x@gmail.com
@@ -64,19 +65,16 @@ const Sidebar = ({ children }: any) => {
                                     </div>
                                     <ul className="py-1" role="none">
                                         <li>
-                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Dashboard</a>
+                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">پنل کاربری</a>
                                         </li>
                                         <li>
-                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Settings</a>
+                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">تنظیمات</a>
                                         </li>
                                         <li>
-                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Earnings</a>
-                                        </li>
-                                        <li>
-                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
+                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">خروج</a>
                                         </li>
                                     </ul>
-                                </div> */}
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -123,8 +121,8 @@ const Sidebar = ({ children }: any) => {
                         </li>
                         <li>
                             <Link href="/login" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <IoIosLogIn className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-                                <span className="flex-1 mr-3 whitespace-nowrap">ورود</span>
+                                <AiOutlineComment className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                                <span className="flex-1 mr-3 whitespace-nowrap">نظرات</span>
                             </Link>
                         </li>
 
